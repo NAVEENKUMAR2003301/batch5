fdaa69c9861c98217e8950ed2effaf01a1e40b52
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-06-24 18:52:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,131 @@
+// variables 
+
+// var
+// let
+// const
+
+// var age = 30; (declaration & initialiazation)
+// var age = 50; (redeclaration)
+// age=80; reintizialation
+
+// var age; assining
+// age=100 reintizialation
+
+
+// console.log(age)
+
+// let newAge = 150;
+// newAge = 200;
+
+// console.log(newAge)
+
+// const employeeName = "XYZ";
+//  employeeName = "ABC"
+
+// console.log(employeeName)
+
+
+// printing statement 
+
+// alert("hello world")
+
+// confirm("enter the world")
+
+// var name = prompt("enter your name :-")
+
+// console.log(name)
+
+
+// document.writeln("hi hello ")
+
+// let name = "ben10"
+
+// console.log(name)
+
+
+// datatypes:-
+
+// 1. primitive datatypes
+// 2. non primitive datatypes
+
+// 1. primitive datatypes 
+
+// 1.Number
+// 2.string
+// 3.boolean
+// 4.null
+// 5.undefined
+
+// 2. non primitive DataTypes 
+
+// 1.Array
+// 2.object
+// 3.function 
+// 4.date
+
+// 1. primitive datatypes :- explain
+
+// console.log(typeof(12));
+// console.log(typeof("123"));
+// console.log(typeof(true));
+// console.log(typeof(num));
+// console.log(typeof(0));
+
+// 2. nonprimitive datatypes
+
+// 1. array 
+
+// let arr = [1,2,3,true,"123",{}]
+
+// console.log(arr[arr.length-2])
+
+// 2. object 
+
+// let object = {
+//     name : "xyz",
+//     age : 14,
+//     gender : "male",
+//     qualification : "btech",
+//     ispassedOut : true,
+//     frnd : {
+//         name : "abc",
+//         age : 14,
+//         ispassedOut : false
+//     },
+//     isCertificate : ["html","css","js"]
+// }
+
+// console.log(object.frnd.ispassedOut);
+
+
+// operators 
+
+// 1. arithmatic operator (mathematical values)
+// 2.Assignment operators (var declration and assign)
+// 3.comparitive operators 1<2
+// 4.logical operators (condition)
+
+// 1. arithmatic operator (mathematical values)
+//  explain 
+
+// addition +
+// subraction -
+// multiplication *
+// division /
+// modulus %
+// increment ++
+// decrement --
+
+console.log(10+20);
+console.log(10-20);
+console.log(10*20);
+console.log(10/20);
+console.log(19%20);
+
+var a = 10
+var b = ++a
+var c = a++
+
+
+console.log(c)
+
