9b5610499a42cbd48b685d3365b72c75a306f1a1
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-06-25 18:29:27 +0530

    operators

--- script.js
+++ script.js
@@ -116,16 +116,161 @@
 // increment ++
 // decrement --
 
-console.log(10+20);
-console.log(10-20);
-console.log(10*20);
-console.log(10/20);
-console.log(19%20);
+// console.log(10+20);
+// console.log(10-20);
+// console.log(10*20);
+// console.log(10/20);
+// console.log(19%20);
 
-var a = 10
-var b = ++a
-var c = a++
+// var a = 10
+// var b = --a
+// var c = a--
 
 
-console.log(c)
+// console.log(c)
 
+// let num = 20
+// let num1 = num-- - num-- 
+
+// console.log(num1);
+
+// Assignment opperator 
+
+// addition 
+
+// let age = 20
+
+
+
+// age += 20
+
+// console.log(age);
+
+// let age1 = 20
+// let additionalVal = 100
+
+// age1 %= 100 
+
+// console.log(age1);
+
+
+// relational operators or comoparision operators 
+
+{/* < less then 5<2 false
+> greater then 5>2 true
+<= less then equal 5<=2 false
+>= greater then equal 5>=2 true
+== loosy type equal 5=="5" true
+=== strictly type equal 5==="5" false */}
+
+
+// console.log(5===5);
+
+
+// logical operators 
+
+// and &&  true && true = true
+
+// or  ||   true || false = true
+
+// not  !  true = false / false = true
+
+
+
+// let ageBen = 10
+// console.log(ageBen>=18 && ageBen<=30);
+
+// console.log(ageBen === 10);
+
+
+
+// concatenation /
+
+// let str1 =  "RCB"
+// let str2 = "get the cup last year"
+
+// console.log(str1+" "+str2);
+
+// template string 
+
+// console.log(`who is last year win the IPL : ${str1} ${str2}.`)
+
+
+// type convertion
+
+// 1.implicit type convertion 
+// 2. explicit type converstion 
+
+// 1. explain 
+
+// let str5 = "123"
+// let num2 = 123
+
+// console.log((10-"67"));
+
+// 2. explain
+
+// console.log(Boolean());
+
+
+
+
+// 1.conditional statement (if, if-else,if else else-if,nested if , switch, ternary operator)
+// 2.looping statement (for , while , do while , for of , for in)
+
+
+
+// 1. explain 
+
+if (false) {
+    console.log("hi")
+}
+
+if(false){
+    console.log("hello")
+}else{
+    console.log("namste")
+}
+
+if(false){
+  console.log("1");
+  
+}else if(0){
+ console.log("2");
+ 
+}else{
+    console.log("3");
+    
+}
+
+if(true){
+    if(true){
+        if(true){
+            console.log("selected")
+        }else{
+
+        }
+    }else{
+
+    }
+}else{
+
+}
+
+
+let hour = 16;
+if(hour >= 1 && hour <= 6){
+    console.log("early morning");
+    
+}else if(hour >= 7 && hour <= 12){
+    console.log("morning")
+}else if(hour >= 13 && hour <= 15){
+    console.log("afternoon");
+    
+}else if(hour >= 16 && hour <= 18){
+    console.log("evening");
+    
+}else{
+    console.log("good night");
+    
+}
